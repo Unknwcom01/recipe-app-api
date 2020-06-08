234d7dc68b496ec07da9ec08d386c5da91f71061
course <66364319+Unknwcom01@users.noreply.github.com>
2020-06-09 00:28:01 +0100

    DB availability testing and postgres introduction

---
+++ app/core/management/__init__.py
@@ -0,0 +1,2 @@
+# additional module named __init__.py on core / management path level
+# this is the second module
---
+++ app/core/management/commands/__init__.py
@@ -0,0 +1,2 @@
+# additional module named __init__.py on core / management / command path level
+# this is the second file
